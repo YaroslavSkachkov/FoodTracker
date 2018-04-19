f6af7ebc280ac66753ad1feb6ac914402fd7c5ab
Yaroslav Skachkov <ikanom@mail.ru>
2018-04-19 18:52:56 +0300

    Tests Remaking

--- FoodTrackerTests/FoodTrackerTests.swift
+++ FoodTrackerTests/FoodTrackerTests.swift
@@ -16,26 +16,26 @@ class FoodTrackerTests: XCTestCase {
     // Confirm that the Meal initializer returns a Meal object when passed valid parameters.
     func testMealInitializationSucceeds() {
         // Zero rating
-        let zeroRatingMeal = Meal.init(name: "Zero", photo: nil, rating: 0)
+        let zeroRatingMeal = Meal.init(name: "Zero", photo: nil, rating: 0, comments: [])
         XCTAssertNotNil(zeroRatingMeal)
         
         // Highest positive rating
-        let positiveRatingMeal = Meal.init(name: "Positive", photo: nil, rating: 5)
+        let positiveRatingMeal = Meal.init(name: "Positive", photo: nil, rating: 5, comments: [])
         XCTAssertNotNil(positiveRatingMeal)
     }
     
     // Confirm that the Meal initialier returns nil when passed a negative rating or an empty name.
     func testMealInitializationFails() {
         // Negative rating
-        let negativeRatingMeal = Meal.init(name: "Negative", photo: nil, rating: -1)
+        let negativeRatingMeal = Meal.init(name: "Negative", photo: nil, rating: -1, comments: [])
         XCTAssertNil(negativeRatingMeal)
         
         // Rating exceeds maximum
-        let largeRatingMeal = Meal.init(name: "Large", photo: nil, rating: 6)
+        let largeRatingMeal = Meal.init(name: "Large", photo: nil, rating: 6, comments: [])
         XCTAssertNil(largeRatingMeal)
         
         // Empty String
-        let emptyStringMeal = Meal.init(name: "", photo: nil, rating: 0)
+        let emptyStringMeal = Meal.init(name: "", photo: nil, rating: 0, comments: [])
         XCTAssertNil(emptyStringMeal)
     }
     
